7c19ae8e772b606b1d48734e342d81f1d3efa4a1
Lev Mezhibovskii <levchik2005@gmail.com>
2021-06-08 12:27:30 +0300

    скруглил углы

--- script/open-block.js
+++ script/open-block.js
@@ -2,6 +2,7 @@ document.getElementById('button-cells').onclick = function() {
     if (document.getElementById('fn__grid').classList.contains('future-news__grid-pressed')) {
     document.getElementById('fn__grid').classList.remove('future-news__grid-pressed');
     document.getElementById('button-cells').classList.remove('button__rotate');
+    document.getElementById('unique__cell').classList.add('unique__cell');
     var elements = document.getElementsByClassName("table__cell");
     for (let i=3; i<9;i++){
         elements[i].classList.add("odd__cells");
@@ -10,6 +11,7 @@ document.getElementById('button-cells').onclick = function() {
     else {
         document.getElementById('fn__grid').classList.add('future-news__grid-pressed');
         document.getElementById('button-cells').classList.add('button__rotate');
+        document.getElementById('unique__cell').classList.remove('unique__cell');
         var elements = document.getElementsByClassName("odd__cells");
         for (let i=0; i<6;i++){
             elements[0].classList.remove("odd__cells");
